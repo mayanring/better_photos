f502264484e11f6fb22ef3a633899279d9230734
Ryan Ming <ryan.ming@gmail.com>
2015-10-23 21:39:01 -0400

    parse f00px response and show images in view

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -3,13 +3,20 @@ class RecommendationsController < ApplicationController
     user_id = photo_params
 
     # this is the query engine, not the event engine
-    # client = PredictionIO::EngineClient.new(ENV['PIO_ENGINE_URL'])
+    client = PredictionIO::EngineClient.new(ENV['PIO_ENGINE_URL'])
     client = PredictionIO::EngineClient.new("http://localhost:8000")
     recommended_photos = client.send_query(user: user_id)
 
     photo_ids = recommended_photos["itemScores"].map { |item| item["item"] }
 
-    @response = F00px.get("photos?ids=#{photo_ids.join(',')}")
+    response = F00px.get("photos?image_size=20&ids=#{photo_ids.join(',')}")
+    json_response = JSON.parse(response.body)
+
+    photos = json_response["photos"]
+
+    @image_urls = photo_ids.map do |photo_id|
+      photos[photo_id.to_s]['images'][0]['url']
+    end
   end
 
   private
